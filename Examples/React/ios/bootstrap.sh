e7ae7e34bd3d1e14212262c80746c1f53a49286f
Eduardo Jesus do Nascimento <eduybx@gmail.com>
2023-10-06 20:11:26 -0300

    chore: add bootstrap based on react-native from node_modules

--- Examples/React/ios/bootstrap.sh
+++ Examples/React/ios/bootstrap.sh
@@ -4,10 +4,10 @@ set -x
 # Note: on this release, there's an issue with this commit.
 # this should be patched on somehow
 # https://github.com/facebook/react-native/pull/28946
-VERSION="0.63.3"
+VERSION="0.72.5"
 
 cleanup() {
-    rm -rf react-native-${VERSION}.*
+    rm -rf react-native-${VERSION}*
 }
 
 download_react_if_necessary() {
@@ -16,25 +16,29 @@ download_react_if_necessary() {
         return
     fi
 
-    PODIR=Vendor
-    mkdir -p PodsHost
-    echo $VERSION > PodsHost/RN_VERSION
-    [[ -f react-native-${VERSION}.zip ]] || \
-        curl -L https://github.com/facebook/react-native/archive/v${VERSION}.zip \
-        -o react-native-${VERSION}.zip
-    unzip -qu react-native-${VERSION}.zip
+    # PODIR=Vendor
+    # mkdir -p PodsHost
+    # echo $VERSION > PodsHost/RN_VERSION
+    # [[ -f react-native-${VERSION}.zip ]] || \
+    #     curl -L https://github.com/facebook/react-native/archive/v${VERSION}.zip \
+    #     -o react-native-${VERSION}.zip
+    # unzip -qu react-native-${VERSION}.zip
 
-    mkdir -p $PODIR
-    [[ ! -d $PODIR/React ]] \
-        || rm -rf $PODIR/React
-    mv react-native-${VERSION} $PODIR/React
+    # mkdir -p $PODIR
+    # [[ ! -d $PODIR/react-native ]] \
+    #     || rm -rf $PODIR/react-native
+    # mv react-native-${VERSION}/packages/react-native $PODIR/react-native
+
+    if [ ! -d ./Vendor/react-native ]; then
+        ln -s ../../node_modules/react-native ./Vendor/React
+    fi
 
     # prepare_command is unsupported these are right out of the podspecs
     sed -i '' \
         's,spec.prepare_command,#spec.prepare_command,g' \
-        Vendor/React/third-party-podspecs/glog.podspec
+        Vendor/react-native/third-party-podspecs/glog.podspec
 }
 
 trap cleanup EXIT
 download_react_if_necessary
-make update_xcodeproj gen_podfile_deps
+# make update_xcodeproj gen_podfile_deps
